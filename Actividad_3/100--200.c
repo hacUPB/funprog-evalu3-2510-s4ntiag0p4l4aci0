c4b39c85c64c33462decabdb9e4f3eba49f43540
Diego <diego.rodriguezr@upb.edu.co>
2025-03-14 09:03:48 -0500

    se añaden ejercicios de clase

---
+++ Actividad_3/100--200.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+int main()
+{
+    int contador =99; 
+    
+    while (contador >= -199) {
+        
+        printf("iteración : %d\n", contador);
+    
+    contador -=2;
+    }
+    return 0;
+}
